e30d1ef4cff42e71d383ea99a5050faacf54b61f
jodufour <jodufour@student.42.fr>
2021-09-20 18:04:06 +0200

    Updated gen.sh

--- gen.sh
+++ gen.sh
@@ -2,8 +2,55 @@
 
 SIZE=$1
 
-gen4() {
+gen2() {
+	for col_up_0 in $(seq 2) ; do
+	for col_up_1 in $(seq 2) ; do
+		for col_down_0 in $(seq 2) ; do
+		for col_down_1 in $(seq 2) ; do
+			for row_left_0 in $(seq 2) ; do
+			for row_left_1 in $(seq 2) ; do
+				for row_right_0 in $(seq 2) ; do
+				for row_right_1 in $(seq 2) ; do
+					echo "$col_up_0 $col_up_1 $col_down_0 $col_down_1 $row_left_0 $row_left_1 $row_right_0 $row_right_1"
+				done
+				done
+			done
+			done
+		done
+		done
+	done
+	done
+}
 
+gen3() {
+	for col_up_0 in $(seq 3) ; do
+	for col_up_1 in $(seq 3) ; do
+	for col_up_2 in $(seq 3) ; do
+		for col_down_0 in $(seq 3) ; do
+		for col_down_1 in $(seq 3) ; do
+		for col_down_2 in $(seq 3) ; do
+			for row_left_0 in $(seq 3) ; do
+			for row_left_1 in $(seq 3) ; do
+			for row_left_2 in $(seq 3) ; do
+				for row_right_0 in $(seq 3) ; do
+				for row_right_1 in $(seq 3) ; do
+				for row_right_2 in $(seq 3) ; do
+					echo "$col_up_0 $col_up_1 $col_up_2 $col_down_0 $col_down_1 $col_down_2 $row_left_0 $row_left_1 $row_left_2 $row_right_0 $row_right_1 $row_right_2"
+				done
+				done
+				done
+			done
+			done
+			done
+		done
+		done
+		done
+	done
+	done
+	done
+}
+
+gen4() {
 	for col_up_0 in $(seq 4) ; do
 	for col_up_1 in $(seq 4) ; do
 	for col_up_2 in $(seq 4) ; do
@@ -39,7 +86,11 @@ gen4() {
 	done
 }
 
-if [[ $SIZE -eq "4" ]] ; then
+if [[ $SIZE -eq "2" ]] ; then
+	gen2
+elif [[ $SIZE -eq "3" ]] ; then
+	gen3
+elif [[ $SIZE -eq "4" ]] ; then
 	gen4
 fi
 
